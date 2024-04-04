91b2c936b4867bac9c0bf633215ffd7bf60c6bda
JAO300 <jonadal02@gmail.com>
2024-04-04 13:34:08 +0000

    test

--- src/main/java/is/vidmot/AskrifandiDialog.java
+++ src/main/java/is/vidmot/AskrifandiDialog.java
@@ -21,7 +21,7 @@ import java.io.IOException;
 public class AskrifandiDialog extends Dialog<Askrifandi> {
 
     // viðmótshlutur
-    //prufa
+    //prufagi
     @FXML
     protected TextField fxNafn; // nafnið á áskrifanda
 
